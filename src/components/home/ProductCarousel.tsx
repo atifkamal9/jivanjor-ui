45b558d21936200989572a3cbea6962260115977
hafizaatifkamal <hafizaatifkamal@gmail.com>
2026-07-08 18:47:31 +0530

    fea: add link to product details and improve layout responsiveness

--- src/components/home/ProductCarousel.tsx
+++ src/components/home/ProductCarousel.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import Image from "next/image";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
@@ -143,23 +144,25 @@ export default function ProductCarousel({ items }: ProductCarouselProps) {
             key={`${card.title}-${idx}`}
             className="overflow-visible! px-1"
           >
-            <div className="flex flex-col items-center relative px-4 mt-8">
-              <div className="absolute aspect-44/51 top-[-30%] w-41 h-48 xl:w-55 xl:h-63 object-contain z-100">
+            <div className="flex flex-col items-center relative px-4 mt-6">
+              <Link
+                href="/products"
+                className="absolute aspect-44/51 group top-[-28%] w-41 h-48 xl:w-55 xl:h-63 object-contain transition-opacity duration-300 ease-out z-100"
+              >
                 {/* Floating image */}
                 <Image
+                  fill
                   src={card.image}
                   alt={card.title}
-                  fill
-                  className="object-contain z-10"
+                  className="object-contain z-10 group-hover:-translate-y-1 transition-all duration-300"
                   priority
-                  // className="absolute aspect-44/51 top-[-28%] w-60 h-65 object-contain z-100"
                 />
-              </div>
+              </Link>
               {/* Card */}
               <div
-                className={`${card.color} relative rounded-[28px] flex-1 w-69 min-h-76 lg:w-69 lg:h-93 lg:min-h-88`}
+                className={`${card.color} relative rounded-[28px] flex-1 w-69 min-h-76 lg:w-69 lg:h-93 lg:min-h-86`}
               >
-                <div className="absolute bottom-2.5 flex flex-col items-center justify-stretch p-6 text-white">
+                <div className="absolute bottom-0 flex flex-1 flex-col items-center justify-stretch pb-10 px-6 text-white">
                   <h3 className="text-2xl font-semibold text-center my-4 pb-4 px-12 border-b">
                     {card.title}
                   </h3>
