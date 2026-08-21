9a39d2180542a628fcf04f61275813c5c67417f7
atifkamal9 <atif.razonova@gmail.com>
2026-08-21 09:56:46 +0530

    feat: update breakpoint values and adjust component heights for improved responsiveness

--- src/components/ui/Subtitle.tsx
+++ src/components/ui/Subtitle.tsx
@@ -14,7 +14,7 @@ export default function Subtitle({
 }: SubtitleProps) {
   return (
     <Component
-      className={`font-google-sans text-base sm:text-xl xd:text-[22px] leading-normal ${className}`}
+      className={`font-google-sans text-base sm:text-xl leading-normal ${className}`}
       {...props}
     >
       {children}
